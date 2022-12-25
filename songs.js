b76dde9cf4ee190d42efca29e5e6b677c6bc43d5
valebcg <valeria.cortes.g@gmail.com>
2022-12-25 19:51:11 -0300

    endpoint route is created

--- songs.js
+++ songs.js
@@ -14,3 +14,11 @@ app.listen(3000, console.log("¡Servidor encendido!"))
 // --- pasando como argumento el middleware ----
 app.use(cors())
 app.use(express.json())
+
+// -----Creando endpoint y ruta-----
+app.get("/songs", async (req, res) => {
+    const songs = JSON.parse(await fsPromises.readFile("repertorio.json", "utf8"))
+    res.json(songs)
+    });
+    
+    
